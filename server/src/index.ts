0500f0b4dbe7b2d07650aab22756f5394ab65b22
John C. Kennedy <devjckennedy@gmail.com>
2020-07-13 20:45:19 +0900

    setup server

---
+++ server/src/index.ts
@@ -0,0 +1,27 @@
+import * as dotenv from "dotenv"
+import * as express from "express"
+import * as bodyParser from "body-parser"
+
+import { connect } from "./db"
+import * as router from "./router/index"
+
+dotenv.config()
+const app = express()
+const { PORT = 4000 } = process.env
+
+// MongoDB
+connect()
+
+app.use(express.static("public"))
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+router.routes( app )
+
+app.listen(PORT, () => {
+  console.log("App is running at %d", PORT)
+})
