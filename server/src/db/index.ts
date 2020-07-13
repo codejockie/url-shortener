49f322d76a66dfacb7da8e44d3ef32ed5c0371ff
John C. Kennedy <devjckennedy@gmail.com>
2020-07-13 20:39:38 +0900

    database connection

---
+++ server/src/db/index.ts
@@ -0,0 +1,40 @@
+import * as mongoose from "mongoose"
+import { LinkModel } from "./links/links.model"
+
+let database: mongoose.Connection
+const { MONGODB_URI = "mongodb://localhost:27017/url-shortener" } = process.env
+
+export const connect = () => {
+  if (database) {
+    return
+  }
+
+  mongoose.connect(MONGODB_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+  })
+
+  database = mongoose.connection
+
+  database.once("open", async () => {
+    console.log("Connected to database")
+  })
+
+  database.on("error", () => {
+    console.log("Error connecting to database")
+  })
+
+  return {
+    LinkModel,
+  }
+}
+
+export const disconnect = () => {
+  if (!database) {
+    return
+  }
+
+  mongoose.disconnect()
+}
