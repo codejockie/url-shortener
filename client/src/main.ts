101bd9cba284f0b0ce5e8fee41f5c59ba8683286
John C. Kennedy <devjckennedy@gmail.com>
2020-07-14 22:19:06 +0900

    fix api base url

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,21 +1,20 @@
-import Vue from "vue"
-import axios from "axios"
-import App from "./App.vue"
-import store from "./store"
-import router from "./router"
-import VueAxios from "vue-axios"
-import "./registerServiceWorker"
+import Vue from "vue";
+import axios from "axios";
+import App from "./App.vue";
+import store from "./store";
+import router from "./router";
+import VueAxios from "vue-axios";
+import "./registerServiceWorker";
 
 const axiosInstance = axios.create({
-  baseURL:
-    process.env.NODE_ENV != "production" ? "http://localhost:4000/api" : "",
-})
+  baseURL: process.env.VUE_APP_API_URL ?? "http://localhost:4000/api",
+});
 
-Vue.use(VueAxios, axiosInstance)
-Vue.config.productionTip = false
+Vue.use(VueAxios, axiosInstance);
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: (h) => h(App),
-}).$mount("#app")
+}).$mount("#app");
