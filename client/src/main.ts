1784c08e860552e4b269244c8c7546b586ee8b81
John C. Kennedy <devjckennedy@gmail.com>
2020-07-14 17:28:45 +0900

    configure axios

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,13 +1,21 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import Vue from "vue"
+import axios from "axios"
+import App from "./App.vue"
+import store from "./store"
+import router from "./router"
+import VueAxios from "vue-axios"
+import "./registerServiceWorker"
 
-Vue.config.productionTip = false;
+const axiosInstance = axios.create({
+  baseURL:
+    process.env.NODE_ENV != "production" ? "http://localhost:4000/api" : "",
+})
+
+Vue.use(VueAxios, axiosInstance)
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount("#app");
+  render: (h) => h(App),
+}).$mount("#app")
