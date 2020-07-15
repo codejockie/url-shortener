a2a38a94316e052d5dd0d0f4f92891cb04d84ee1
John C. Kennedy <devjckennedy@gmail.com>
2020-07-15 18:46:29 +0900

    create links service

---
+++ client/src/services/links.ts
@@ -0,0 +1,14 @@
+import axios from './axios';
+
+export class LinksService {
+  getLinks() {
+    return axios.get("/links");
+  }
+
+  shortenLink(url: string) {
+    return axios.post("/links", { url });
+  }
+}
+
+// A singleton instance
+export default new LinksService();
