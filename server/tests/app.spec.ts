733574d96680bebab1e1db7c754ac81a41f38519
John C. Kennedy <devjckennedy@gmail.com>
2020-07-16 01:02:11 +0900

    write app test

---
+++ server/tests/app.spec.ts
@@ -0,0 +1,11 @@
+import * as request from "supertest"
+import app from "@/app"
+
+describe("App", () => {
+  test("app works fine", async () => {
+    const result = await request(app).get("/status")
+
+    expect(result.status).toEqual(200)
+    expect(result.body.status).toEqual("OK")
+  })
+})
