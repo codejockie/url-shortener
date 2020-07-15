b437f1b0b2fab52121b2f540d8e48a42fb85bb81
John C. Kennedy <devjckennedy@gmail.com>
2020-07-15 18:45:04 +0900

    update jest.config.js

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel"
+  collectCoverage: true,
+  coveragePathIgnorePatterns: [
+    "node_modules",
+    "test-config",
+    "interfaces",
+    "jestGlobalMocks.ts",
+    ".module.ts",
+    "<rootDir>/src/app/main.ts",
+    ".mock.ts",
+    "<rootDir>/tests/helper.ts",
+    "<rootDir>/src/services/axios.ts",
+  ],
+  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1"
+  }
 };
