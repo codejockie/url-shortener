fd77e3627f31337d43986b9508c7933314afca47
John C. Kennedy <devjckennedy@gmail.com>
2020-07-16 01:01:44 +0900

    configure jest

---
+++ server/jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  collectCoverage: true,
+  coveragePathIgnorePatterns: [
+    "node_modules",
+    "test-config",
+    "interfaces",
+    "jestGlobalMocks.ts",
+    ".module.ts",
+    "<rootDir>/src/database/links/links.methods.ts",
+    "<rootDir>/src/database/links/links.statics.ts",
+    ".mock.ts",
+  ],
+  coverageReporters: ["html", "json", "lcov", "text"],
+  moduleFileExtensions: ["ts", "js"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  testEnvironment: "node",
+  testRegex: "/tests/.*.spec.ts",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+}
