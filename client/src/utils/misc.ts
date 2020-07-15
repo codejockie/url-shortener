93624dfb9161d9884dd7c1215d6f2141c6e6ddd0
John C. Kennedy <devjckennedy@gmail.com>
2020-07-15 18:47:05 +0900

    update copy util

--- client/src/utils/misc.ts
+++ client/src/utils/misc.ts
@@ -1,6 +1,6 @@
 export function copy(url: string) {
   const listener = (ev: ClipboardEvent) => {
-    ev?.preventDefault();
+    ev.preventDefault();
     ev?.clipboardData?.setData("text/plain", url);
   };
   document.addEventListener("copy", listener);
