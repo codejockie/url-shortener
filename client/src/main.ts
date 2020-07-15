5c2e741ed1e37748f855d44242c2c3191072852e
John C. Kennedy <devjckennedy@gmail.com>
2020-07-15 18:50:37 +0900

    remove vue-axios

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,16 +1,9 @@
 import Vue from "vue";
-import axios from "axios";
 import App from "./App.vue";
 import store from "./store";
 import router from "./router";
-import VueAxios from "vue-axios";
 import "./registerServiceWorker";
 
-const axiosInstance = axios.create({
-  baseURL: process.env.VUE_APP_API_URL ?? "http://localhost:4000/api",
-});
-
-Vue.use(VueAxios, axiosInstance);
 Vue.config.productionTip = false;
 
 new Vue({
