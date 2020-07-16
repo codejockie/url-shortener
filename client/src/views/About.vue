d6a3be9e0572b4254aa43316076790848181496a
John C. Kennedy <devjckennedy@gmail.com>
2020-07-16 13:42:40 +0900

    update About page

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Short.ly</h1>
+    <div>
+      <p>At Short.ly, we look after your links so you do not have to. We are committed to helping you get the best from your links</p>
+    </div>
   </div>
 </template>
+<style lang="scss" scoped>
+.about {
+  margin-top: 71px;
+  padding: 0 20px;
+  text-align: center;
+
+  & h1 {
+    padding-top: 20px;
+  }
+}
+</style>
