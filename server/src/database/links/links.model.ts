8a450313da71ce8e705d797451fd7d3e08c6ff77
John C. Kennedy <devjckennedy@gmail.com>
2021-02-04 10:40:11 +0100

    pretty links model

--- server/src/database/links/links.model.ts
+++ server/src/database/links/links.model.ts
@@ -2,7 +2,4 @@ import * as mongoose from "mongoose"
 import LinkSchema from "./links.schema"
 import { ILinkDocument, ILinkModel } from "./links.types"
 
-export const LinkModel = mongoose.model<ILinkDocument>(
-  "Link",
-  LinkSchema
-) as ILinkModel
+export const LinkModel = mongoose.model<ILinkDocument>("Link", LinkSchema) as ILinkModel
