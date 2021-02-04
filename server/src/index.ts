acd3ccdb7f38bac78e28d48d06f19484ad8e9b5e
John C. Kennedy <devjckennedy@gmail.com>
2021-02-04 09:55:22 +0100

    add winston and error handler middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,36 @@
 import * as dotenv from "dotenv"
-
-import app from "./app"
+import * as winston from "winston"
+import { app } from "./app"
 import { connectDatabase } from "./database"
+import { winstonLogger } from "./utils/logger"
 
 dotenv.config()
 const { PORT = 4000 } = process.env
+const logger = winstonLogger("index")
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  )
+}
 
 // MongoDB
 connectDatabase()
 
+// Error Handler
+app.use((err, req, res, next) => {
+  logger.error(err.message)
+  err.statusCode = err.statusCode || 500
+  err.status = err.status || "error"
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  })
+})
+
 app.listen(PORT, () => {
-  console.log("App is running at %d", PORT)
+  logger.info(`App is running on localhost:${PORT}`)
 })
