3f3d89bea0afafc5460a594c7c2332afbb5e196f
John C. Kennedy <devjckennedy@gmail.com>
2021-02-04 10:39:33 +0100

    use extract link id middleware

--- server/src/router/index.ts
+++ server/src/router/index.ts
@@ -1,15 +1,13 @@
 import { Router } from "express"
 import * as linksController from "@/controllers/links.controller"
+import { extractLinkId } from "@/middleware"
 
 export default () => {
   const router = Router()
-  router
-    .route("/links")
-    .get(linksController.getLinks)
-    .post(linksController.shortenLink)
-  router
-    .route("/links/:urlId")
-    .get(linksController.getLinkById)
+  router.route("/links").get(linksController.getLinks).post(linksController.shortenLink)
+
+  router.param("linkId", extractLinkId)
+  router.route("/links/:linkId").get(linksController.getLinkById)
 
   return router
 }
